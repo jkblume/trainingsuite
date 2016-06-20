1ad09c30241170859cf234fb09e32a6145738df5
Jakob Blume <jakob.blume@mailbox.org>
2016-06-20 23:32:29 +0200

    first view of progress

--- publish/views.py
+++ publish/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import Training
 # Create your views here.
+
+
 def home(request):
-    return render(request, 'publish/home.html', {})
+    training = Training.objects.first()
+    return render(request, 'publish/home.html', {'training':training})
